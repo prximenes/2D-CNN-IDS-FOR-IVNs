e3952b80a95a77b78797a79c104ef8569d3d2b44
Pedro Carmo <prxc@cin.ufpe.br>
2022-05-24 13:39:18 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 for file in /autoeth-intrusion-dataset/pcap_files/csvs/*
 do 
-	tshark -r "$file" -T fields -e frame.number -e frame.protocols -e iec61883.videodata -E separator=, -E occurrence=f > "/home/pedro/Documentos/ufpe/mestrado/automotives/autoeth-intrusion-dataset/pcap_files/csvs/${file##*/}.csv"
+	tshark -r "$file" -T fields -e frame.number -e frame.protocols -e iec61883.videodata -E separator=, -E occurrence=f > "/autoeth-intrusion-dataset/pcap_files/csvs/${file##*/}.csv"
 done
 
 
