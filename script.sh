22ba6ca8463903d2ceaf68372cc14e754e0cc65d
Pedro Carmo <prxc@cin.ufpe.br>
2022-05-24 13:32:44 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,7 @@
+for file in /home/pedro/Documentos/ufpe/mestrado/automotives/autoeth-intrusion-dataset/pcap_files/csvs/*
+do 
+	tshark -r "$file" -T fields -e frame.number -e frame.protocols -e iec61883.videodata -E separator=, -E occurrence=f > "/home/pedro/Documentos/ufpe/mestrado/automotives/autoeth-intrusion-dataset/pcap_files/csvs/${file##*/}.csv"
+done
+
+
+
